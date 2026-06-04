0ddefc94e571f92249d5d8111c95ab8e23efffdc
claudiasilvanarj-ops <claudia.silvana.rj@gmail.com>
2026-06-04 14:00:53 -0300

    feat: add Vercel serverless function for TanStack Start

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+import { createStartHandler, defaultRenderHandler } from '@tanstack/start/server'
+import { createRouter } from '../../src/router'
+
+export default createStartHandler({
+  createRouter,
+})(defaultRenderHandler)
