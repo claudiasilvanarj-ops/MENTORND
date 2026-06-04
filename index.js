2509af5b4bcec30172ee7b56075550e1cb7121c3
claudiasilvanarj-ops <claudia.silvana.rj@gmail.com>
2026-06-04 17:26:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Bem-vinda ao MentorND</h1>
+      <p>Seu app está rodando no Vercel com sucesso!</p>
+    </div>
+  );
+}
