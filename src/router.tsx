88d7be6efd906feed82a1d89b4363afe6eaca3fc
claudiasilvanarj-ops <claudia.silvana.rj@gmail.com>
2026-06-04 12:31:33 -0300

    ANCORAGEM ROUTER
    ANCORAGEM ROUTER

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,1 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
 
-export function createRouter() {
-  return createTanStackRouter({
-    routeTree,
-    defaultPreload: 'intent',
-  })
-}
-
-export const router = createRouter()
-
-export const getRouter = () => router
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof createRouter>
-  }
-}
