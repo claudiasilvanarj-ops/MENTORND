8fcc1b09fb622bd18e7f9ccf48d039a6555145ff
claudiasilvanarj-ops <claudia.silvana.rj@gmail.com>
2026-06-04 17:57:27 -0300

    Update index.tsx 3

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,8 @@
-
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Bem-vinda ao MentorND</h1>
       <p>App React com TanStack Start rodando no Vercel!</p>
     </div>
-  )
+  );
 }
-
